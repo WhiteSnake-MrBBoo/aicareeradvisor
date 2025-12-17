333aeaf8f2b1c5488cb8f1119273beb2c408147f
WhiteSnake-MrBBoo <mrbulsapabb@gmail.com>
2025-12-17 11:38:47 +0900

    --기본 환경 설정-- 1.DB : MariaDB 2.IDE : Intellij 2025.02 3.Version : JDK 21 / SpringBoot 3.58 4.Server : Localhost

---
+++ src/main/java/com/example/ai_career_advisor/AiCareerAdvisorApplication.java
@@ -0,0 +1,13 @@
+package com.example.ai_career_advisor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCareerAdvisorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiCareerAdvisorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ai_career_advisor/AiCareerAdvisorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ai_career_advisor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCareerAdvisorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
